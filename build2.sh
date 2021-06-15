f016566a783e9752f71bb3ee0c67c7b03c6eff46
aman25502 <65896145+aman25502@users.noreply.github.com>
2021-06-15 18:54:26 +0530

    collect GAPPS build ccache

--- build2.sh
+++ build2.sh
@@ -30,29 +30,29 @@ export CCACHE_EXEC=$(which ccache)
 #echo "export USE_CCACHE=1" >> ~/.bashrc
 #source ~/.bashrc
 export USE_CCACHE=1
-export WITH_GAPPS=false
-export GAPPS_BUILD=false
-export BLISS_BUILD_VARIANT=VANILLA
+export WITH_GAPPS=true
+export GAPPS_BUILD=true
+export BLISS_BUILD_VARIANT=GAPPS
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 ccache -M 20G # It took less than 6 GB for less than 2 hours in 2 builds for Samsung A10
 #ccache -o compression=true # Will save times and data to download and upload ccache, also negligible performance issue
 ccache -z
 
-#tg_sendText "Started Collecting CCACHE....."
+tg_sendText "Started Collecting CCACHE....."
 
 # Next 9 lines should be run first to collect ccache and then upload, after doning it 1 or 2 times, our ccache will help to build without these 8 lines.
 lunch bliss_mojito-userdebug
 #make api-stubs-docs || echo no problem, we need ccache
 #make system-api-stubs-docs || echo no problem we need ccache
 #make test-api-stubs-docs || echo no problem, we need ccache
-#blissify mojito | tee build.txt & # dont remove that '&'
-#sleep 85m
-#kill %1
+blissify mojito | tee build.txt & # dont remove that '&'
+sleep 85m
+kill %1
 #ccache -s
 #and dont use below codes for first 1 or 2 times, to get ccache uploaded,
 
-tg_sendText "Starting Compilation.."
+#tg_sendText "Starting Compilation.."
 
 # Compilation by parts if you get RAM issue but takes nore time!
 #mka api-stubs-docs -j8
@@ -60,7 +60,7 @@ tg_sendText "Starting Compilation.."
 #mka test-api-stubs-docs -j8
 #mka bacon -j8 | tee build.txt
 
-blissify mojito | tee build.txt
+#blissify mojito | tee build.txt
 #ccache -a
 
 (ccache -s && echo '' && free -h && echo '' && df -h && echo '' && ls -a out/target/product/mojito/) | tee final_monitor.txt
